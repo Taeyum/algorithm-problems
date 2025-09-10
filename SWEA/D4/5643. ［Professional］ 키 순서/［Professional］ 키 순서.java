6dd2bb6b76a0eb703e52d3fd3011bbfdb4238380
Taeyum <131154104+Taeyum@users.noreply.github.com>
2025-09-10 12:10:24 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,852 ms, Memory: 97,940 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -3,22 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 정보들을 인접 행렬에 넣기
-// 나보다 작은 애들 찾기
-// 나보다 큰 애들 찾기
-// 나보다 작은애 + 큰애 = 전체 학생 수 - 1 이면 내 키 번호를 알 수 있다?
-
 public class Solution {
 	static int N;
 	static int M;
-	static int[][] list;
-	static int smallCnt;
-	static int upperCnt;
-	static int cnt;
+	static int[][] adjList;
+	static int up;
+	static int down;
+	static int totalCnt;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
 		
 		for(int test_case = 1 ; test_case <= T ; test_case++) {
@@ -26,46 +20,48 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			st = new StringTokenizer(br.readLine());
 			M = Integer.parseInt(st.nextToken());
-			cnt = 0;
-			list = new int[N + 1][N + 1];
-			
+			totalCnt = 0;
+			adjList = new int[N + 1][N + 1];
+
 			for(int i = 0 ; i < M ; i++) {
-				st = new StringTokenizer(br.readLine(), " ");
+				st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
-				list[from][to] = 1;
+				adjList[from][to] = 1;	
 			}
 			
-			for(int i = 1 ; i < N + 1; i++) {
-				upperCnt = 0;
-				smallCnt = 0;
-				findUpper(i, new boolean[N+1]);
-				findSmall(i, new boolean[N+1]);
-				if(upperCnt + smallCnt == N - 1) {
-					cnt++;
+			for(int i = 1 ; i <= N ; i++) {
+				up = 0;
+				down = 0;
+				upperCnt(i,new boolean[N + 1]);
+				downCnt(i,new boolean[N + 1]);
+				if(up + down == N - 1) {
+					totalCnt++;
 				}
 			}
-			System.out.println("#" + test_case + " " + cnt);
-		}
-	}
-	
-	static void findSmall(int to, boolean[] visited) {
-		visited[to] = true;
-		for(int i = 1 ; i < N + 1 ; i++) {
-			if(!visited[i] && list[i][to] == 1) {
-				findSmall(i, visited);
-				smallCnt++;
-			}
+			
+			System.out.println("#"+test_case + " " +totalCnt);
+			
+			
 		}
 		
 	}
-	
-	static void findUpper(int from, boolean[] visited) {
+	static void upperCnt(int from, boolean[] visited) {
 		visited[from] = true;
-		for(int i = 1 ; i < N + 1 ; i++) {
-			if(!visited[i] && list[from][i] == 1) {
-				findUpper(i, visited);
-				upperCnt++;
+		for(int i = 1 ; i <= N ; i++) {
+			if(!visited[i] && adjList[from][i] == 1) {
+				upperCnt(i, visited);
+				up++;
+			}
+		}
+	}
+	
+	static void downCnt(int to, boolean[] visited) {
+		visited[to] = true;
+		for(int i = 1 ; i <= N ; i++) {
+			if(!visited[i] && adjList[i][to] == 1) {
+				downCnt(i, visited);
+				down++;
 			}
 		}
 	}
