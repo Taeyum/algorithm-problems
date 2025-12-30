33b09a937db62a8b3ac14d5b67e60ceb572b1047
Taeyum <131154104+Taeyum@users.noreply.github.com>
2025-12-30 19:57:23 +0900

    [Bronze III] Title: 친구 친구, Time: 560 ms, Memory: 194416 KB -BaekjoonHub

---
+++ 백준/Bronze/10865. 친구 친구/친구 친구.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            arr[A]++;
+            arr[B]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
