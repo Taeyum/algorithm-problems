2005505f9891fb7f00843cfe3b1d2fbe65fe90e5
Taeyum <131154104+Taeyum@users.noreply.github.com>
2026-01-12 22:38:55 +0900

    [Bronze III] Title: 최댓값, Time: 168 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int max = 0;
+        int idx = 0;
+        
+        for(int i = 1; i <= 9; i++) {
+            int num = sc.nextInt();
+            if(num > max) {
+                max = num;
+                idx = i;
+            }
+        }
+        
+        System.out.println(max);
+        System.out.println(idx);
+    }
+}
