bc8ff263b8638544dc7bf5563d0a0f4c34504739
Taeyum <131154104+Taeyum@users.noreply.github.com>
2026-01-15 21:48:08 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 1796 ms, Memory: 34364 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// A, B 존재, A 는 B 를 먹음 (A > B 일때만 가능)
+// A 의 크기가 {8,1,7,3,1} 일 때 B의 크기가 {3,6,1} 일때 A가 B를 먹을 수 있는 쌍의 개수는 8-3, 8-6, 8-1, 7-3, 7-6, 7-1, 3-1
+public class Main {
+    static int count;
+    static int[] A,B;
+    static BufferedReader br;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            init(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+            System.out.println(result());
+        }
+    }
+
+    static void init(int aIdx, int bIdx) throws IOException{
+        A = new int[aIdx];
+        B = new int[bIdx];
+        count = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < aIdx; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < bIdx; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+    }
+    // 1 1 3 7 8
+    // 1 3 6
+    static int result() {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if(A[i] <= B[j]) continue;
+                if(A[i] > B[j]) count++;
+                else break;
+            }
+        }
+
+        return count;
+    }
+}
