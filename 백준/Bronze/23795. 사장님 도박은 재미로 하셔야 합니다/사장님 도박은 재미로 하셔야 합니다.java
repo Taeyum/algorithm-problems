8cd6cc2a0e2889261c597750eec4f900ab641279
Taeyum <131154104+Taeyum@users.noreply.github.com>
2026-01-31 23:22:28 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 204 ms, Memory: 21604 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long sum = 0;
+
+        while (true) {
+            int money = sc.nextInt();
+            if (money == -1) {
+                break;
+            }
+            sum += money;
+        }
+
+        System.out.println(sum);
+    }
+}
